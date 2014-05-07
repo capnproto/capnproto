4302fe29d84064d5c55d8ea9bc21b92353840737
Philip <p@partylemon.com>
2014-05-07 17:40:31 +1200

    Allow direct access to the memory of a primitive list via. an ArrayPtr.

--- c++/src/capnp/list.h
+++ c++/src/capnp/list.h
@@ -119,6 +119,8 @@ struct List<T, Kind::PRIMITIVE> {
     inline Iterator begin() const { return Iterator(this, 0); }
     inline Iterator end() const { return Iterator(this, size()); }
 
+    inline kj::ArrayPtr<const T> asPtr() const { return reader.template asPtr<T>(); }
+
   private:
     _::ListReader reader;
     template <typename U, Kind K>
@@ -159,6 +161,8 @@ struct List<T, Kind::PRIMITIVE> {
     typedef _::IndexingIterator<Builder, T> Iterator;
     inline Iterator begin() { return Iterator(this, 0); }
     inline Iterator end() { return Iterator(this, size()); }
+
+    inline kj::ArrayPtr<T> asPtr() { return builder.template asPtr<T>(); }
 
   private:
     _::ListBuilder builder;
