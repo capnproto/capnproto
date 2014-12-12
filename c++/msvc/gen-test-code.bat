1557496057f48df8ba2001183e3386e13a485380
Kenton Varda <kenton@sandstorm.io>
2014-12-12 02:24:19 -0800

    Remove obsolete MSVC stuff.

--- c++/msvc/gen-test-code.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-rem You'll need to build capnp.exe and capnpc-c++.exe using MinGW.
-
-capnp compile -oc++ -I../src --src-prefix=../src ../src/capnp/test.capnp ../src/capnp/test-import.capnp ../src/capnp/test-import2.capnp
