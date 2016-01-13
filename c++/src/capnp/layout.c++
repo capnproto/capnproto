capnproto/capnproto

commit 2c9c5c83854f04d4dbc15499d9aaa6470b0cb082

Account for the possibility of adopting an empty struct.

c++/src/capnp/layout.c++
@@ -896,10 +896,15 @@ struct WireHelpers {
 
     if (dstSegment == srcSegment) {
       // Same segment, so create a direct pointer.
-      dst->setKindAndTarget(srcTag->kind(), srcPtr, dstSegment);
 
-      // We can just copy the upper 32 bits.  (Use memcpy() to comply with aliasing rules.)
-      memcpy(&dst->upper32Bits, &srcTag->upper32Bits, sizeof(srcTag->upper32Bits));
+      if (srcTag->kind() == WirePointer::STRUCT && srcTag->structRef.wordSize() == 0) {
+        dst->setKindAndTargetForEmptyStruct();
+      } else {
+        dst->setKindAndTarget(srcTag->kind(), srcPtr, dstSegment);
+
+        // We can just copy the upper 32 bits.  (Use memcpy() to comply with aliasing rules.)
+        memcpy(&dst->upper32Bits, &srcTag->upper32Bits, sizeof(srcTag->upper32Bits));
+      }
     } else {
       // Need to create a far pointer.  Try to allocate it in the same segment as the source, so
       // that it doesn't need to be a double-far.

c++/src/capnp/orphan-test.c++
@@ -48,6 +48,16 @@ TEST(Orphans, Structs) {
   checkTestMessage(root.asReader().getStructField());
 }
 
+TEST(Orphans, EmptyStruct) {
+  MallocMessageBuilder builder;
+  auto root = builder.initRoot<test::TestAnyPointer>();
+  auto anyPointer = root.getAnyPointerField();
+  EXPECT_TRUE(anyPointer.isNull());
+  auto orphan = builder.getOrphanage().newOrphan<test::TestEmptyStruct>();
+  anyPointer.adopt(kj::mv(orphan));
+  EXPECT_FALSE(anyPointer.isNull());
+}
+
 TEST(Orphans, Lists) {
   MallocMessageBuilder builder;
   auto root = builder.initRoot<TestAllTypes>();

commit 4d4f831a3d71fc483035971fb6763da221bddcfe

Fix message corruption bug introduced in 2c9c5c8.
As of that change, adopting an empty struct into a pointer that already had some other value fails to overwrite the upper 32 bits of the pointer, thus resulting in a struct pointer with totally bogus bounds. Normally this will either cause a bounds check exception to be thrown when that pointer is later accessed, or lead to a struct containing bogus data (but if teh struct was expected to be empty, probably that data is never accessed).

c++/src/capnp/layout.c++
@@ -897,14 +897,14 @@ struct WireHelpers {
     if (dstSegment == srcSegment) {
       // Same segment, so create a direct pointer.
 
-      if (srcTag->kind() == WirePointer::STRUCT && srcTag->structRef.wordSize() == 0) {
+      if (srcTag->kind() == WirePointer::STRUCT && srcTag->structRef.wordSize() == 0 * WORDS) {
         dst->setKindAndTargetForEmptyStruct();
       } else {
         dst->setKindAndTarget(srcTag->kind(), srcPtr, dstSegment);
-
-        // We can just copy the upper 32 bits.  (Use memcpy() to comply with aliasing rules.)
-        memcpy(&dst->upper32Bits, &srcTag->upper32Bits, sizeof(srcTag->upper32Bits));
       }
+
+      // We can just copy the upper 32 bits.  (Use memcpy() to comply with aliasing rules.)
+      memcpy(&dst->upper32Bits, &srcTag->upper32Bits, sizeof(srcTag->upper32Bits));
     } else {
       // Need to create a far pointer.  Try to allocate it in the same segment as the source, so
       // that it doesn't need to be a double-far.

c++/src/capnp/orphan-test.c++
@@ -55,6 +55,19 @@ TEST(Orphans, EmptyStruct) {
   EXPECT_TRUE(anyPointer.isNull());
   auto orphan = builder.getOrphanage().newOrphan<test::TestEmptyStruct>();
   anyPointer.adopt(kj::mv(orphan));
+  EXPECT_EQ(0, anyPointer.targetSize().wordCount);
+  EXPECT_FALSE(anyPointer.isNull());
+}
+
+TEST(Orphans, EmptyStructOverwrite) {
+  MallocMessageBuilder builder;
+  auto root = builder.initRoot<test::TestAnyPointer>();
+  auto anyPointer = root.getAnyPointerField();
+  EXPECT_TRUE(anyPointer.isNull());
+  anyPointer.initAs<TestAllTypes>();
+  auto orphan = builder.getOrphanage().newOrphan<test::TestEmptyStruct>();
+  anyPointer.adopt(kj::mv(orphan));
+  EXPECT_EQ(0, anyPointer.targetSize().wordCount);
   EXPECT_FALSE(anyPointer.isNull());
 }
 