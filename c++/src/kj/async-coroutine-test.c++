5eaa80b194d9731c9f36b6609d541d5b1c87e04b
Harris Hancock <harris@cloudflare.com>
2022-10-07 15:53:51 +0100

    Import coCapture() from workerd
    Since the defect this feature works around applies to all lambda coroutines, it makes sense to live in libkj.

--- c++/src/kj/async-coroutine-test.c++
+++ c++/src/kj/async-coroutine-test.c++
@@ -501,6 +501,77 @@ KJ_TEST("HttpClient to capnproto.org with a coroutine") {
   promise.wait(io.waitScope);
 }
 
+// =======================================================================================
+// coCapture() tests
+
+KJ_TEST("Verify coCapture() functors can only be run once") {
+  auto io = kj::setupAsyncIo();
+
+  auto functor = coCapture([](kj::Timer& timer) -> kj::Promise<void> {
+    co_await timer.afterDelay(1 * kj::MILLISECONDS);
+  });
+
+  auto promise = functor(io.lowLevelProvider->getTimer());
+  KJ_EXPECT_THROW(FAILED, functor(io.lowLevelProvider->getTimer()));
+
+  promise.wait(io.waitScope);
+}
+
+auto makeDelayedIntegerFunctor(size_t i) {
+  return [i](kj::Timer& timer) -> kj::Promise<size_t> {
+    co_await timer.afterDelay(1 * kj::MILLISECONDS);
+    co_return i;
+  };
+}
+
+KJ_TEST("Verify coCapture() with local scoped functors") {
+  auto io = kj::setupAsyncIo();
+
+  constexpr size_t COUNT = 100;
+  kj::Vector<kj::Promise<size_t>> promises;
+  for (size_t i = 0; i < COUNT; ++i) {
+    auto functor = coCapture(makeDelayedIntegerFunctor(i));
+    promises.add(functor(io.lowLevelProvider->getTimer()));
+  }
+
+  for (size_t i = COUNT; i > 0 ; --i) {
+    auto j = i-1;
+    auto result = promises[j].wait(io.waitScope);
+    KJ_REQUIRE(result == j);
+  }
+}
+
+auto makeCheckThenDelayedIntegerFunctor(kj::Timer& timer, size_t i) {
+  return [&timer, i](size_t val) -> kj::Promise<size_t> {
+    KJ_REQUIRE(val == i);
+    co_await timer.afterDelay(1 * kj::MILLISECONDS);
+    co_return i;
+  };
+}
+
+KJ_TEST("Verify coCapture() with continuation functors") {
+  // This test usually works locally without `coCapture()()`. It does however, fail in
+  // ASAN.
+  auto io = kj::setupAsyncIo();
+
+  constexpr size_t COUNT = 100;
+  kj::Vector<kj::Promise<size_t>> promises;
+  for (size_t i = 0; i < COUNT; ++i) {
+    auto promise = io.lowLevelProvider->getTimer().afterDelay(1 * kj::MILLISECONDS).then([i]() {
+      return i;
+    });
+    promise = promise.then(coCapture(
+        makeCheckThenDelayedIntegerFunctor(io.lowLevelProvider->getTimer(), i)));
+    promises.add(kj::mv(promise));
+  }
+
+  for (size_t i = COUNT; i > 0 ; --i) {
+    auto j = i-1;
+    auto result = promises[j].wait(io.waitScope);
+    KJ_REQUIRE(result == j);
+  }
+}
+
 #endif  // KJ_HAS_COROUTINE
 
 }  // namespace
--- c++/src/kj/async.h
+++ c++/src/kj/async.h
@@ -586,6 +586,114 @@ inline CaptureByMove<Func, Decay<MovedParam>> mvCapture(MovedParam&& param, Func
 }
 
 // =======================================================================================
+// Hack for safely using a lambda as a coroutine.
+
+#if KJ_HAS_COROUTINE
+
+namespace _ {
+
+template<typename Functor>
+struct CaptureForCoroutine {
+  kj::Maybe<Functor> maybeFunctor;
+
+  explicit CaptureForCoroutine(Functor&& f) : maybeFunctor(kj::mv(f)) {}
+
+  template<typename ...Args>
+  static auto coInvoke(Functor functor, Args&&... args)
+      -> decltype(functor(kj::fwd<Args>(args)...)) {
+    // Since the functor is now in the local scope and no longer a member variable, it will be
+    // persisted in the coroutine state.
+
+    // Note that `co_await functor(...)` can still return `void`. It just happens that
+    // `co_return voidReturn();` is explicitly allowed.
+    co_return co_await functor(kj::fwd<Args>(args)...);
+  }
+
+  template<typename ...Args>
+  auto operator()(Args&&... args) {
+    KJ_IREQUIRE(maybeFunctor != nullptr,
+        "Attempted to invoke CaptureForCoroutine functor multiple times");
+    auto localFunctor = kj::mv(*kj::_::readMaybe(maybeFunctor));
+    maybeFunctor = nullptr;
+    return coInvoke(kj::mv(localFunctor), kj::fwd<Args>(args)...);
+  }
+};
+
+}  // namespace _
+
+template <typename Functor>
+auto coCapture(Functor&& f) {
+  // Assuming `f()` returns a Promise<T> `p`, wrap `f` in such a way that it will outlive its
+  // returned Promise. Note that the returned object may only be invoked once.
+  //
+  // This function is meant to help address this pain point with functors that return a coroutine:
+  // https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rcoro-capture
+  //
+  // The two most common patterns where this may be useful look like so:
+  // ```
+  // void addTask(Value myValue) {
+  //   auto myFun = [myValue]() -> kj::Promise<void> {
+  //     ...
+  //     co_return;
+  //   };
+  //   tasks.add(myFun());
+  // }
+  // ```
+  // and
+  // ```
+  // kj::Promise<void> afterPromise(kj::Promise<void> promise, Value myValue) {
+  //   auto myFun = [myValue]() -> kj::Promise<void> {
+  //     ...
+  //     co_return;
+  //   };
+  //   return promise.then(kj::mv(myFun));
+  // }
+  // ```
+  //
+  // Note that there are potentially more optimal alternatives to both of these patterns:
+  // ```
+  // void addTask(Value myValue) {
+  //   auto myFun = [](auto myValue) -> kj::Promise<void> {
+  //     ...
+  //     co_return;
+  //   };
+  //   tasks.add(myFun(myValue));
+  // }
+  // ```
+  // and
+  // ```
+  // kj::Promise<void> afterPromise(kj::Promise<void> promise, Value myValue) {
+  //   auto myFun = [&]() -> kj::Promise<void> {
+  //     ...
+  //     co_return;
+  //   };
+  //   co_await promise;
+  //   co_await myFun();
+  //   co_return;
+  // }
+  // ```
+  //
+  // For situations where you are trying to capture a specific local variable, kj::mvCapture() can
+  // also be useful:
+  // ```
+  // kj::Promise<void> reactToPromise(kj::Promise<MyType> promise) {
+  //   BigA a;
+  //   TinyB b;
+  //
+  //   doSomething(a, b);
+  //   return promise.then(kj::mvCapture(b, [](TinyB b, MyType type) -> kj::Promise<void> {
+  //     ...
+  //     co_return;
+  //   });
+  // }
+  // ```
+
+  return _::CaptureForCoroutine(kj::mv(f));
+}
+
+#endif  // KJ_HAS_COROUTINE
+
+// =======================================================================================
 // Advanced promise construction
 
 class PromiseRejector: private AsyncObject {
