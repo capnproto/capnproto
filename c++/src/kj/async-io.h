b16f4f4b32f8c8167c769cbba68395e4318d35f5
Fabio Rossetto <fabio.rossetto@zhinst.com>
2025-03-17 10:16:48 +0100

    Expose socketpair creation in async-io
    AsyncIoProvider has a "newPipeThread" function that spawns a thread
    that can be communicated with through the returned pipe.
    
    Users can't always change the way threads in their application are
    spawned, so "newPipeThread" can't always be used. In those cases,
    it is better to give users access to the lower-level tools and let them
    create the pipe themselves.
    
    By exposing newOsSocketpair(), it is fairly easy to setup cross-thread
    communication.

--- c++/src/kj/async-io.h
+++ c++/src/kj/async-io.h
@@ -22,6 +22,7 @@
 #pragma once
 
 #include "async.h"
+#include "kj/memory.h"
 #include <kj/function.h>
 #include <kj/thread.h>
 #include <kj/timer.h>
@@ -899,6 +900,16 @@ public:
   // Convenience wrappers which transfer ownership via AutoCloseFd (Unix) or AutoCloseHandle
   // (Windows). TAKE_OWNERSHIP will be implicitly added to `flags`.
 };
+
+template <typename T> struct Socketpair_ { T fds[2]; };
+using Socketpair = Socketpair_<LowLevelAsyncIoProvider::OwnFd>;
+// We use a template to work around the fact that LowLevelAsyncIoProvider::OwnFd
+// is an incomplete type, without having to include the io.h header.
+
+Socketpair newOsSocketpair();
+// Creates a socket pair, using socketpair(2) on Unix-like systems.
+// On Windows, which doesn't have a built-in socketpair(), a loopback
+// TCP connection is used.
 
 Own<AsyncIoProvider> newAsyncIoProvider(LowLevelAsyncIoProvider& lowLevel);
 // Make a new AsyncIoProvider wrapping a `LowLevelAsyncIoProvider`.
